d181137cd595724e61b7c12d7eac03d9ebced172
Mike Rossetti <s2t2@users.noreply.github.com>
2019-06-09 13:15:06 -0400

    Solve price parsing

--- app/json_parser.py
+++ app/json_parser.py
@@ -1,5 +1,6 @@
-
+from pprint import pprint
 import os
+import json
 
 def to_usd(my_price):
     """
@@ -8,14 +9,25 @@ def to_usd(my_price):
     return "${0:,.2f}".format(my_price) #> $12,000.71
 
 def latest_closing_price(my_json_filepath):
-    return "131.4000"
 
+    with open(my_json_filepath, "r") as json_file:
+        file_contents = json_file.read()
 
+    daily_prices = json.loads(file_contents)
 
+    latest_date = daily_prices["Meta Data"]["3. Last Refreshed"]
 
+    latest_prices = daily_prices["Time Series (Daily)"][latest_date]
 
+    latest_close = latest_prices["4. close"] #> '1066.0400'
+
+    return latest_close
 
 if __name__ == "__main__":
+
+    #
+    # CAPTURE USER INPUTS
+    #
 
     selected_symbol = input("Please select a stock symbol ('AAPL', GOOG', or 'MSFT'):")
 
@@ -25,10 +37,16 @@ if __name__ == "__main__":
 
     print("SELECTED SYMBOL:", selected_symbol.upper())
 
-    selected_filepath = os.path.join(os.path.dirname(__file__), "..", "data", f"daily_prices_{selected_symbol.lower()}.json")
+    #
+    # PROCESS DATA AND DISPLAY OUTPUTS
+    #
 
+    selected_filepath = os.path.join(os.path.dirname(__file__), "..", "data", f"daily_prices_{selected_symbol.lower()}.json")
     print("PARSING A LOCAL JSON FILE:", selected_filepath)
 
-    latest_close = latest_closing_price(selected_filepath)
+    if not os.path.isfile(selected_filepath):
+        print("OH, THAT FILE DOESN'T EXIST. PLEASE PLACE A FILE THERE OR CHECK YOUR FILEPATH...")
+        exit()
 
+    latest_close = latest_closing_price(selected_filepath)
     print("LATEST CLOSING PRICE:", to_usd(float(latest_close)))
--- test/json_parser_test.py
+++ test/json_parser_test.py
@@ -1,23 +1,10 @@
 import os
 
-from app.json_parser import to_usd, latest_closing_price
+from app.json_parser import latest_closing_price, to_usd
 
 def test_processing_of_user_inputs():
     assert "goog".upper() == "GOOG"
     assert "GOOG".lower() == "goog"
-
-def test_conversion_of_prices():
-    assert float("131.4000") == 131.4
-
-def test_to_usd():
-    # it should apply USD formatting:
-    assert to_usd(4.50) == "$4.50"
-    # it should display two decimal places:
-    assert to_usd(4.5) == "$4.50"
-    # it should round to two places:
-    assert to_usd(4.55555) == "$4.56"
-    # it should display thousands separators:
-    assert to_usd(1234567890.5555555) == "$1,234,567,890.56"
 
 def test_latest_closing_price():
 
@@ -29,3 +16,19 @@ def test_latest_closing_price():
 
     goog_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "daily_prices_goog.json")
     assert latest_closing_price(goog_filepath) == "1066.0400"
+
+def test_to_usd():
+    # it should apply USD formatting:
+    assert to_usd(4.50) == "$4.50"
+
+    # it should display two decimal places:
+    assert to_usd(4.5) == "$4.50"
+
+    # it should round to two places:
+    assert to_usd(4.55555) == "$4.56"
+
+    # it should display thousands separators:
+    assert to_usd(1234567890.5555555) == "$1,234,567,890.56"
+
+    # it wants you to convert a string to a float before passing the value in:
+    assert to_usd(float("131.4000")) == "$131.40"
